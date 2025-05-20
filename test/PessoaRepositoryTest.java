4ba58c26ca75d6cb448eeeafad579be094dcc43d
Maria Antonya Fernandes da Silva <mariaantonyafernandes@gmail.com>
2025-05-20 15:06:31 -0300

    Update PessoaRepositoryTest.java

--- test/PessoaRepositoryTest.java
+++ test/PessoaRepositoryTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.*;
 public class PessoaRepositoryTest {
     private static PessoaRepository repo;
 
-    // vai executar uma vez antes de todos os testes
     @BeforeClass
     public static void conectarAoBancoFake() {
         System.out.println("==> Inicio testes...\n");
